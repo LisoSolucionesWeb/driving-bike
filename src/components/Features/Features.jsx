12b1b43c06030be579e873a577cb8ef1b669ff46
IgnaDev <arcortoon@gmail.com>
2024-06-04 17:36:16 -0300

    feat(ui): crear componente Features y mejorar carrusel en Products
    - Se creó y estilizó el componente Features.
    - Se ajustaron los componentes Product y Products.
    - El carrusel en Products ahora se mueve automáticamente.
    - Se añadieron los iconos necesarios.

---
+++ src/components/Features/Features.jsx
@@ -0,0 +1,33 @@
+import "./style.css"
+
+
+export default function Features() {
+  return (
+    <div className="container-Feature">
+      <div className="feature">
+        <img src="https://media.revistagq.com/photos/5cd5804bd1dca86286c0929f/16:9/w_2560%2Cc_limit/mejores%2520bicicletas%2520de%2520montan%25CC%2583a.jpg" alt="" />
+        <h2>Montaña</h2>
+      </div>
+      <div className="feature">
+        <img src="https://img.redbull.com/images/c_limit,w_1500,h_1000,f_auto,q_auto/redbullcom/2018/04/09/77c228ec-91f4-4a1c-a27f-b427c47baa7d/tu-primera-carrera" alt="" />
+        <h2>Carreras</h2>
+      </div>
+      <div className="feature">
+        <img src="https://ciclismoepico.com/wp-content/uploads/2024/02/cascos-ciclismo-mujer.jpg" alt="" />
+        <h2>Cascos</h2>
+      </div>
+      <div className="feature">
+        <img src="https://www.siroko.com/blog/c/app/uploads/2021/09/verano-1440x900.jpg.webp" alt="" />
+        <h2>Ropa</h2>
+      </div>
+      <div className="feature">
+        <img src="https://www.shutterstock.com/image-photo/bicycle-cycling-accessories-600nw-1027172386.jpg" alt="" />
+        <h2>Accesorios</h2>
+      </div>
+      <div className="feature">
+        <img src="https://images.unsplash.com/photo-1620802051782-725fa33db067?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZWxlY3RyaWMlMjBiaWtlfGVufDB8fDB8fHww" alt="" />
+        <h2>E-Bikes</h2>
+      </div>
+    </div>
+  )
+}
